cab6ecb91fdfade435f5cd3d7973ee401013d703
Bahodir <choriyevbahodir0>
2024-04-20 11:48:16 +0500

    yet to become responsive

---
+++ main.js
@@ -0,0 +1,193 @@
+const nextBtn = document.querySelector(".arrow-right");
+const backBtn = document.querySelector(".arrow-left");
+const coffeCards = document.querySelectorAll(".fst");
+const wrapper = document.querySelector(".choose-wrapper");
+
+const content = [
+	`<div class="first fst">
+	<img
+		class="choice"
+		src="Assets/HomePage/coffee-slider-1.png"
+		alt=""
+	/>
+	<div class="desc-wrap">
+		<h2 class="price-name">S’mores Frappuccino</h2>
+		<p class="description">
+			This new drink takes an espresso and mixes it with brown sugar and
+			cinnamon before being topped with oat milk.
+		</p>
+		<h2 class="price-name">$5.50</h2>
+		<div class="borders">
+			<div class="borderr darkened"></div>
+			<div class="borderr"></div>
+			<div class="borderr"></div>
+		</div>
+	</div>
+</div>`,
+
+	`	<div class="second fst">
+	<img
+		class="choice"
+		src="Assets/HomePage/coffee-slider-2.png"
+		alt=""
+	/>
+	<div class="desc-wrap">
+		<h2 class="price-name">Caramel Macchiato</h2>
+		<p class="description">
+			Fragrant and unique classic, espresso with rich caramel-peanut
+			syrup, with cream under whipped thick foam.
+		</p>
+		<h2 class="price-name">$5.00</h2>
+		<div class="borders">
+			<div class="borderr"></div>
+			<div class="borderr darkened"></div>
+			<div class="borderr"></div>
+		</div>
+	</div>
+</div>`,
+
+	`	<div class="third fst">
+	<img
+		class="choice"
+		src="Assets/HomePage/coffee-slider-3.png"
+		alt=""
+	/>
+	<div class="desc-wrap">
+		<h2 class="price-name">Ice coffe</h2>
+		<p class="description">
+			A popular summer drink that tones and invigorates. Prepared from
+			coffee, milk and ice.
+		</p>
+		<h2 class="price-name">$4.50</h2>
+		<div class="borders">
+			<div class="borderr"></div>
+			<div class="borderr"></div>
+			<div class="borderr darkened"></div>
+		</div>
+	</div>
+</div>`,
+];
+
+let index = 0;
+
+nextBtn.addEventListener("click", () => {
+	if (index == 2) return;
+	index++;
+	if (index > 2) return;
+	wrapper.innerHTML = content[index];
+});
+
+backBtn.addEventListener("click", () => {
+	if (index == 0) return;
+	index--;
+	if (index < 0) return;
+	wrapper.innerHTML = content[index];
+});
+
+const btn = document.querySelector(".btn");
+const menuT = document.querySelector(".menu-t");
+const menuCup = document.querySelector(".menu-cup");
+const appIcons = document.querySelectorAll(".app-icon");
+const btnIcons = document.querySelectorAll(".btn-icon");
+const typeIcons = document.querySelectorAll(".type-icon");
+const coffeeWraps = document.querySelectorAll(".coffee-items-wrap");
+const homePage = document.querySelector(".homePageWrapper");
+const menuPage = document.querySelector(".menuPageWrapper");
+const menuBtns = document.querySelectorAll(".menuBtn");
+const clickEvents = document.querySelectorAll(".clickEvent");
+const forAbsolutes = document.querySelectorAll(".forAbsolute");
+const closeBtns = document.querySelectorAll(".closeBtn");
+const overlay = document.querySelector(".overlay");
+
+btn.addEventListener("mouseover", () => {
+	menuCup.style.transform = "scale(1)";
+	menuT.style.marginLeft = "-7px";
+	menuCup.classList.add("add-cup");
+});
+btn.addEventListener("mouseleave", () => {
+	menuCup.style.transform = "scale(0)";
+	menuT.style.marginLeft = "7px";
+	menuCup.classList.remove("add-cup");
+});
+
+btnIcons.forEach((btnIcon) => {
+	btnIcon.addEventListener("mouseenter", () => {
+		const path = btnIcon.querySelectorAll("path");
+		path.forEach(
+			(el) => (
+				(el.style.fill = "rgba(225, 212, 201, 1)"),
+				(el.style.transition = "0.8s")
+			)
+		);
+	});
+
+	btnIcon.addEventListener("mouseleave", () => {
+		const path = btnIcon.querySelectorAll("path");
+		path.forEach(
+			(el) => ((el.style.fill = ""), (el.style.transition = "0.8s"))
+		);
+	});
+});
+
+typeIcons.forEach((icon, index) => {
+	icon.addEventListener("click", () => {
+		coffeeWraps.forEach((wrap) => (wrap.style.display = "none"));
+		typeIcons.forEach((typeIcon) => {
+			typeIcon.style.backgroundColor = "rgba(225, 212, 201, 1)";
+			typeIcon.style.color = "rgba(64, 63, 61, 1)";
+
+			typeIcon.addEventListener("mouseover", () => {
+				typeIcon.style.backgroundColor = "rgba(102, 95, 85, 1)";
+				typeIcon.style.color = "rgba(225, 212, 201, 1)";
+			});
+			typeIcon.addEventListener("mouseleave", () => {
+				typeIcon.style.backgroundColor = "rgba(225, 212, 201, 1)";
+				typeIcon.style.color = "rgba(64, 63, 61, 1)";
+				// get the color again
+				typeIcons.forEach((typeIcon) => {
+					typeIcon.style.backgroundColor = "rgba(225, 212, 201, 1)";
+					typeIcon.style.color = "rgba(64, 63, 61, 1)";
+				});
+				typeIcons[index].style.backgroundColor = "rgba(102, 95, 85, 1)";
+				typeIcons[index].style.color = "rgba(225, 212, 201, 1)";
+				// got the color again
+			});
+		});
+		typeIcons[index].style.backgroundColor = "rgba(102, 95, 85, 1)";
+		typeIcons[index].style.color = "rgba(225, 212, 201, 1)";
+		coffeeWraps[index].style.display = "flex";
+	});
+});
+
+menuBtns.forEach((menuBtn) => {
+	menuBtn.addEventListener("click", () => {
+		if (menuBtn.innerText == "Menu") {
+			homePage.style.display = "none";
+			menuPage.style.display = "block";
+			menuBtns.forEach((menu) => (menu.children[0].textContent = "Home"));
+		} else {
+			homePage.style.display = "block";
+			menuPage.style.display = "none";
+			menuBtns.forEach((menu) => (menu.children[0].textContent = "Menu"));
+		}
+	});
+});
+
+clickEvents.forEach((clickEvent, index) => {
+	clickEvent.addEventListener("click", () => {
+		forAbsolutes[index].style.display = "block";
+		overlay.style.display = "block";
+	});
+});
+
+closeBtns.forEach((closeBtn) => {
+	closeBtn.addEventListener("click", () => {
+		forAbsolutes.forEach((coffeeWrap) => (coffeeWrap.style.display = "none"));
+		overlay.style.display = "none";
+	});
+});
+
+overlay.addEventListener("click", () => {
+	forAbsolutes.forEach((coffeeWrap) => (coffeeWrap.style.display = "none"));
+	overlay.style.display = "none";
+});
